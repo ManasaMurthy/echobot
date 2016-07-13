2f0df5048f1bd5f92406613c9bd7e6b82855e685
ManasaMurthy <manasa.n.murthy@gmail.com>
2016-07-13 16:33:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var restify = require('restify');
 var builder = require('botbuilder');
+var restify = require('restify');
 
 //=========================================================
 // Bot Setup
@@ -8,9 +8,9 @@ var builder = require('botbuilder');
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+   console.log('%s listening to %s', server.name, server.url);
 });
-  
+
 // Create chat bot
 var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
@@ -23,6 +23,59 @@ server.post('/api/messages', connector.listen());
 // Bots Dialogs
 //=========================================================
 
-bot.dialog('/', function (session) {
-    session.send("Hello World");
-});
+var intents = new builder.IntentDialog();
+bot.dialog('/', intents);
+
+intents.matches(/^change mid/i, [
+    function (session) {
+        session.beginDialog('/profile');
+    },
+    function (session, results) {
+        session.send('Ok... Changed mid to %s', session.userData.mid);
+    }
+]);
+intents.matches(/^cloudbag/i, [
+    function (session, results) {
+		var open = require('open');
+		open('https://cloudbag.mindtree.com/web/login');
+        session.send('cloudbag opened...%s', session.userData.mid);
+    }
+]);
+
+intents.matches(/^list artifacts/i, [
+	function (session) {
+        builder.Prompts.text(session, 'Enter cloud provider');
+    },
+	function (session, results) {
+		session.dialogData.cloud = results.response;
+		builder.Prompts.text(session, 'Enter cloud service');
+		session.dialogData.service = results.response;
+		var open = require('open');
+		open('http://cloudbagdev.southeastasia.cloudapp.azure.com/rest-cloudbag-service/Artifacts?userId='+session.userData.mid
+		+"&cloudProvider="+session.dialogData.cloud +"&cloudService="+session.dialogData.service);
+		session.send('Artifacts listed for %s', session.userData.mid);
+		session.endDialog();
+    }
+]);
+intents.onDefault([
+    function (session, args, next) {
+        if (!session.userData.mid) {
+            session.beginDialog('/profile');
+        } else {
+            next();
+        }
+    },
+    function (session, results) {
+        session.send('Hello %s!', session.userData.mid);
+    }
+]);
+
+bot.dialog('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! Enter your MID?');
+    },
+    function (session, results) {
+        session.userData.mid = results.response;
+        session.endDialog();
+    }
+]);
